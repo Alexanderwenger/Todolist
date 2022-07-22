e7c71924122583409ec997c57b3d03e39373181f
Alexanderwenger <awengern@gmail.com>
2022-07-22 04:06:11 +0000

    tarea incompleta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
 import React from "react";
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+
+	const [tareas, addTarea] = useState([]);
+	const [nuevaTarea, setNuevaTarea] = useState("");
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>To do list</h1>
+			<input value={nuevaTarea} onChange={e => setNuevaTarea(e.target.value)} type="text" placeholder="Tareas nueva?" />
+			<button onClick={() => SumarNuevaTarea()}>Adicionar</button>
+			<ul>
+				{tareas.map((valor, indice) =>
+					<li key={indice}>
+						{valor}
+						<button onClick={() => borrarTarea(indice)}>Borrar</button>
+					</li>)}
+			</ul>
 		</div>
 	);
+
+	function SumarNuevaTarea() {
+		addTarea([...tareas, nuevaTarea]);
+		setNuevaTarea("");
+	}
+
+	function borrarTarea(indice) {
+		let arraytemporal = [...tareas];
+		arraytemporal.splice(indice, 1);
+		addTarea(arraytemporal);
+	}
+
 };
 
 export default Home;
+
+/* {<div className="text-center">
+			<h1>To do list</h1>
+			<input value={nuevaTarea} onChange={e=>setNuevaTarea(e.target.value)} type="text" placeholder="Tareas nueva?" />
+			{tareas.map((valor, indice, arr) => { return <li key={indice}>{valor}
+			<button className="BotonEliminar">x</button></li> })}
+		</div>} */
